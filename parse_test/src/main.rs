8dcfd2fca6b2aa7dfb0fa0c5fed4aac8636d60cd
pshrestha <pranaya.shrestha2@gmail.com>
2023-05-21 09:25:24 -0500

    shadowing

--- parse_test/src/main.rs
+++ parse_test/src/main.rs
@@ -1,14 +1,5 @@
-use std::io;
-
 fn main() {
-    println!("Hello, enter a number");
-
-    let mut test = String::new();
-    io::stdin().read_line(&mut test).expect("Could not read input from user");
-
-    println!("You entered: {test}");
-    println!("Converting string to integer...");
-
-    let test: u8 = test.trim().parse().expect("Please enter a number");
-    println!("The converted integer is: {test}");
+    let shadow = "   ";
+    let shadow: usize = shadow.len();
+    println!("length is: {shadow}");
 }
