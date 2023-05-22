70d2ee056ba1e59375aba4a3167e1352d5300747
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-05-22 16:50:31 -0500

    interactive fetching on array element

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let a = [1,2,3,4,5];
+    let mut index = String::new();
+
+    println!("Please enter which index you want to access:");
+
+    io::stdin().read_line(&mut index).expect("Failed to read line");
+
+    let index: usize = index.trim().parse().expect("Index entered was not a number");
+
+    let element = a[index];
+    println!("Your element at index {index} is: {element}");
 }
