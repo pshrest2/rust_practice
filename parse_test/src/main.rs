8ba1b0270cf47103a6a095ba7eeaa604bf3444ed
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-02 16:00:30 -0500

    will add everything to the main file

--- parse_test/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let tuple: (u32, i64) = (18, -5 * 103948);
-
-    println!("first: {}, second: {}", tuple.0, tuple.1);
-
-    let array: [i32;5] = [1,2,3,4,5];
-    println!("first: {}", array[array.len() - 1]);
-}
