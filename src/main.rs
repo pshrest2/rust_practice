259b5f41f9ed5a324d1fdcbe73c4c6ec9a8b0cfe
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-04 16:28:57 -0500

    rust prevents dangling references with compile time error

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
 fn main() {
-    let mut name = String::from("Pranaya Babu");
-
-    let test = &name;
-
-    let updatable_name = &mut name;
-    let len = length(updatable_name);
-    println!("Length of {updatable_name} is {len}");
-
-    let updatable_name2 = &mut name;
-    let len2 = length(updatable_name2);
-    println!("Length of {updatable_name2} is {len2}");
-
-    println!("Value of test is {test}");
+    let reference_to_nothing = dangle();    
 }
 
-fn length(variable: &mut String) -> usize {
-    variable.push_str(" Shrestha");
-    variable.len()
+fn dangle() -> &String {
+    let s = String::from("some string");
+    &s
 }
