d5ddff52cf180d4083b9181ed38e25794040476e
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-04 16:23:33 -0500

    creating mutable and immutable reference of same value works like this

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 fn main() {
-    let name = String::from("Pranaya Shrestha");
-    println!("Length of {name} is {}", length(&name));
+    let mut name = String::from("Pranaya Babu");
+
+    let test = &name;
+    println!("Value of test is {test}");
+
+    let updatable_name = &mut name;
+    let len = length(updatable_name);
+    println!("Length of {updatable_name} is {len}");
+
+    let updatable_name2 = &mut name;
+    let len2 = length(updatable_name2);
+    println!("Length of {updatable_name2} is {len2}");
+
 }
 
-fn length(variable: &String) -> usize {
-    variable.push_str(" Babu");
+fn length(variable: &mut String) -> usize {
+    variable.push_str(" Shrestha");
     variable.len()
 }
