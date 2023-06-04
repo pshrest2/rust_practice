f3d95ff11c2d03606b51d1066c166f123126cde9
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-04 13:18:09 -0500

    statement and expression functions

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ fn main() {
         y + 20
     };
 
-    another_function(x);
+    display_x(x);
+    println!("The value of y is{}", calculate_y());
 }
 
-fn another_function(x: i8){
+fn display_x(x: i8){
     println!("The value of x is {x}");
+}
+
+fn calculate_y() -> i32 {
+    5
 }
