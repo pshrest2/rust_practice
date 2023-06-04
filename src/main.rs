0c85282a018406d98fc14ed9d1bccf1b2ed5c3e5
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-04 15:52:47 -0500

    invalid code to represent transfer of ownership with function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 fn main() {
-    let mut x = 5;
-    let mut y = x;
-    x += 1;
-    y += 2;
+    let variable = String::from("Pizza");
+    let x = 7;
 
-    println!("Value of x is {x} ");
-    println!("Value of y is {y} ");
+    takes_ownership(variable); // transfer of ownership. variable cannot be used after this line
+    makes_copy(x); // x is cloned into the function. x can be used after this line
 
+    println!("{variable}"); // this will result in compile error
+    println!("{x}");
+}   // x goes out of scope. drop is called on x to free the memory
+    // drop is not called since it's already out of scope
 
-    let mut user1 = String::from("Peter");
-    let mut user2 = user1;
+fn takes_ownership(variable: String){ // variable comes into scope
+    println!("{variable}");
+} // variable goes out of scope. drop called on variable to free the memory
 
-    user1.push_str(" Parker");
-    
-    println!("User 1 is {user1}");
-    println!("User 2 is {user2}");
-}
+fn makes_copy(x: i32){ // x comes into scope
+    println!("{x}");
+} // x goes out of scope. drop is not called
