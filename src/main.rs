2cbc6d478123b0f94e6354e4d68c944f8be03935
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-06-04 15:31:58 -0500

    invalid code to test scopes in rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    for number in (1..4).rev() {
-        print!("{number} ");
+    let mut name = String::from("Pranaya");
+    name.push_str(" Shrestha");
+
+    println!("Full name is {name}");
+
+    {
+        let mut username = String::from("pshrest2");
+        username.push_str("1234");
+        println!("Username is {username}");
     }
-    println!("Take Off");
+    println!("Username outside the scope is {username}");
 }
