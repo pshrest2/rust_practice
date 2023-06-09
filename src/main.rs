ed3f64f4d5e37183337f3e724dadf4a8cd5957b3
pshrestha <pranaya.shrestha2@gmail.com>
2023-06-08 19:01:30 -0500

    use slices in array

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
     let sentence = String::from("  hello world");
     let first_word = first_word(&sentence);
+
     println!("First word is {first_word}");
+
+    let array = [1,2,3,4,5];
+    let sliced_array = &array[1..3];
+    assert_eq!(sliced_array, &[2,3], "Testing that slices matches");
 }
 
-fn first_word(sentence: &String) -> &str {
+fn first_word(sentence: &str) -> &str {
     let trimmed = sentence.trim();
     let bytes = trimmed.as_bytes();
 
