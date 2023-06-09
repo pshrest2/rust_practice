8e53fa210db0ca411040e7b6455a141a8b8b6062
pshrestha <pranaya.shrestha2@gmail.com>
2023-06-09 08:30:07 -0500

    using impl and #derive(Debug) and dbg

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 fn main() {
-    let user = build_user(String::from("agraham"), String::from("agraham@stjude.org"));
-    let is_or_not = if user.active { String::from("is") } else { String::from("is not") };
-    println!("{} {is_or_not} active and has logged in {} times with email {}", user.username, user.sign_in_count, user.email);
+    let object = Rectangle {
+        length: 10,
+        breadth: 10
+    };
+    let area = object.area();
+    println!("The length is {}", object.length);
+    println!("The breadth is {}", object.breadth);
+    println!("Therefore, the area is {area}");
+    println!("object is {:#?}", object);
+    dbg!(&object);
 }
 
-fn build_user (username: String, email: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    breadth: u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.breadth
     }
-}
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64
 }
